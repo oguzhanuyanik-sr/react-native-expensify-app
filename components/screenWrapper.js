721fa821d69a48875972f12901859f97f3e7476f
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2024-06-04 10:14:31 +0300

    project created

---
+++ components/screenWrapper.js
@@ -0,0 +1,12 @@
+import { View, Text, StatusBar } from 'react-native';
+import React from 'react';
+
+export default function ScreenWrapper({ children }) {
+  let statusBarHeight = StatusBar.currentHeight
+    ? StatusBar.currentHeight
+    : Platform.OS == 'ios'
+    ? 30
+    : 0;
+
+  return <View style={{ paddingTop: statusBarHeight }}>{children}</View>;
+}
